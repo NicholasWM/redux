6c03a843c92e1e055e2dcd85e7feb54f62bf25ec
NicholasWM <nicholaswesleymazzei@gmail.com>
2021-10-25 14:26:02 -0300

    add: Reducer actions

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,27 +1,33 @@
 import { Reducer } from "redux"
-import { ICartState } from "./types"
+import { ICartState, IProduct } from "./types"
+import produce from 'immer'
 
 const INITIAL_STATE:ICartState = {
     items: []
 }
 
 const cart:Reducer<ICartState> = (state=INITIAL_STATE, action)=>{
-    console.log(state, action);
-    switch (action.type) {
-        case 'ADD_PRODUCT_TO_CART':
-            const {product} = action.payload
-            return {
-                ...state,
-                items: [
-                    ...state.items,
-                    {product, quantity:1}
-                ]
+    return produce(state, draft => {
+        switch (action.type) {
+            case 'ADD_PRODUCT_TO_CART':
+                const {product} = action.payload
+                const productInCartIndex = draft.items.findIndex(item => 
+                    item.product.id === product.id    
+                )
+
+                if(productInCartIndex >= 0){
+                    draft.items[productInCartIndex].quantity++;
+                }else{
+                    draft.items.push({
+                        product, quantity:1
+                    })
+                }
+                break;
+            default:{
+                return draft
             }
-            break;
-        default:{
-            return state
         }
-    }
+    })
 }
 
 export default cart
